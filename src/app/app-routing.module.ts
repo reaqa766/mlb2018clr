8400be1cd25a38f2810fa775c7e8263763a69758
reaqanvo <reaqanvo@gmail.com>
2018-11-11 20:07:41 -0400

    Organizacion de la pagina para que sea responsive MENU y HOME

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { NewsComponent} from '../app/components/news/news.component';
+import { AboutUsComponent} from '../app/components/about-us/about-us.component';
+import { RegisterComponent} from '../app/components/register/register.component';
+// import { ContactUsComponent} from '../app/components//contact-us/contact-us.component';
+import { HomeComponent} from '../app/components/home/home.component';
+import { ByPositionComponent} from '../app/components/players/by-position/by-position.component';
+import { ByTeamComponent} from '../app/components/players/by-team/by-team.component';
+import { DailyComponent} from '../app/components/players/daily/daily.component';
+import { ActivesComponent} from '../app/components/players/actives/actives.component';
+import { BiographyComponent} from '../app/components/players/biography/biography.component';
+
+const routes: Routes = [
+  { path:'',
+  component: HomeComponent},
+  { path:'news',
+  component: NewsComponent},
+  { path:'aboutUs',
+  component: AboutUsComponent},
+  { path:'register',
+  component: RegisterComponent},
+  { path:'position',
+  component: ByPositionComponent},
+  { path:'team',
+  component: ByTeamComponent},
+  { path:'daily',
+  component: DailyComponent},
+  { path:'actives',
+  component: ActivesComponent},
+  { path:'bioActives',
+  component: BiographyComponent},
+  // { path:'contactUs',
+  // component: ContactUsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -6,14 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  show_menu = false;
+  w: number;
+
   constructor() {
-
-   }
-
-  ngOnInit() {
 
   }
 
+  ngOnInit() {
+    if (window.innerWidth >= 768) {
+      this.show_menu = false;
+      console.log('ancho', this.w);
+    } else {
+      this.show_menu = true;
+      console.log('ancho2', this.w);
+
+    }
+  }
+
+  show_menu_function() {
+    this.show_menu = false;
+
+
+  }
+
+
+  show_menu_function2(event) {
+    console.log(event.target);
+
+    this.show_menu = true;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+     this.w = window.innerWidth;
+    if (this.w >= 768) {
+      this.show_menu = false;
+      console.log('ancho', this.w);
+    } else {
+      this.show_menu = true;
+      console.log('ancho2', this.w);
+
+    }
+  }
 
 
 }
